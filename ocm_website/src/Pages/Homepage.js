e17465121e4706bed0ee36655e4200ee66712e33
Rathore <nitish.rathore25@gmail.com>
2021-08-26 12:17:39 -0400

    Update Homepage.js

--- ocm_website/src/Pages/Homepage.js
+++ ocm_website/src/Pages/Homepage.js
@@ -8,7 +8,6 @@ import MyMenu from "../Components/Menu";
 import axios from 'axios';
 import Button from '@material-ui/core/Button';
 import { Link, Redirect } from 'react-router-dom';
-import MyMenu from "../Components/Menu";
 
 class Homepage extends React.Component {
   constructor() {
